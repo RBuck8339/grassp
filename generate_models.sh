b9cc1b9f0832b64611dc5f1c5fcf9aced63f3d8c
root <root@BLD>
2024-07-15 18:32:19 -0400

    Fixed an issue where .model wouldn't be appended to the file name

--- generate_models.sh
+++ generate_models.sh
@@ -11,6 +11,6 @@ do
     for lambda in "${lambda_labels[@]}"
     do
         echo "Running with seed=$seed and lambda=$lambda"
-        python run.py --edges datasets/_synthetic_n=100_seed=16/_synthetic_n=100_seed=16.edges --model_path residual_synthetic_n=100_seed=16_B=100_lambda=${lambda}_dim=2_epoch=300_spe=10_bs=100_lr=0.1_seed=${seed} --prior_lambda ${lambda} --seed ${seed} --device cpu
+        python run.py --edges datasets/_synthetic_n=100_seed=16/_synthetic_n=100_seed=16.edges --model_path residual_synthetic_n=100_seed=16_B=100_lambda=${lambda}_dim=2_epoch=300_spe=10_bs=100_lr=0.1_seed=${seed}.model --prior_lambda ${lambda} --seed ${seed} --device cpu
     done
 done
